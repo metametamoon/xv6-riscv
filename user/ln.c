f17b25a6df3407bf2198fcb163b5273e749da327
metametamoon <sorvin.levs@gmail.com>
2023-05-18 01:17:22 +0300

    Added ln.c

--- user/ln.c
+++ user/ln.c
@@ -5,11 +5,13 @@
 int
 main(int argc, char *argv[])
 {
-  if(argc != 3){
+  if(argc != 3 && argc != 4){
     fprintf(2, "Usage: ln old new\n");
     exit(1);
   }
-  if(link(argv[1], argv[2]) < 0)
+  if(argc == 3 && link(argv[1], argv[2]) < 0)
     fprintf(2, "link %s %s: failed\n", argv[1], argv[2]);
+  else if(argc == 4 && strcmp(argv[1], "-s") == 0 && symlink(argv[2], argv[3]) < 0)
+    fprintf(2, "symlink %s %s %s: failed\n", argv[1], argv[2], argv[3]);
   exit(0);
 }
